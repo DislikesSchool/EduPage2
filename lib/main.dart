0c463b23ea6d4ae93306845ce8e782a8e9996c52
vyPal <kubik.palacky@gmail.com>
2023-04-10 20:02:20 +0200

    Remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_session_manager/flutter_session_manager.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'home.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
