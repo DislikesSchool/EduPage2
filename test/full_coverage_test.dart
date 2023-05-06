02e3f00238d66f96b6a21694cb533c5dd6ea8e6a
vyPal <kubik.palacky@gmail.com>
2023-05-06 13:31:13 +0200

    Added command for including all lib files

---
+++ test/full_coverage_test.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: unused_import
+import 'package:eduapge2/firebase_options.dart';
+import 'package:eduapge2/grades.dart';
+import 'package:eduapge2/home.dart';
+import 'package:eduapge2/homework.dart';
+import 'package:eduapge2/icanteen.dart';
+import 'package:eduapge2/icanteen_setup.dart';
+import 'package:eduapge2/load.dart';
+import 'package:eduapge2/login.dart';
+import 'package:eduapge2/main.dart';
+import 'package:eduapge2/message.dart';
+import 'package:eduapge2/messages.dart';
+import 'package:eduapge2/timetable.dart';
+
+void main() {}
