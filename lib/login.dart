8ed84e554d164a52c7837fd2f0ad8a0c47e3da8a
vyPal <kubik.palacky@gmail.com>
2023-08-29 07:22:28 +0200

    Modify login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -3,7 +3,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  final String err;
+  const LoginPage({super.key, required this.err});
 
   @override
   State<StatefulWidget> createState() => LoinPageState();
@@ -14,6 +15,7 @@ class LoinPageState extends State<LoginPage> {
   late SharedPreferences sharedPreferences;
   String email = "";
   String password = "";
+  String server = "";
 
   @override
   void initState() {
@@ -48,6 +50,7 @@ class LoinPageState extends State<LoginPage> {
                 ),
               ),
               Text(local!.loginUseExistingCredentials),
+              if (widget.err != "") Text(widget.err),
               TextField(
                 decoration: InputDecoration(
                   icon: const Icon(Icons.email),
@@ -65,10 +68,20 @@ class LoinPageState extends State<LoginPage> {
                 obscureText: true,
                 keyboardType: TextInputType.visiblePassword,
               ),
+              if (widget.err != "")
+                TextField(
+                  decoration: InputDecoration(
+                    icon: const Icon(Icons.cloud_queue),
+                    hintText: local!.loginServer,
+                  ),
+                  onChanged: (text) => {server = text},
+                  keyboardType: TextInputType.url,
+                ),
               ElevatedButton(
                 onPressed: () => {
                   sharedPreferences.setString("email", email),
                   sharedPreferences.setString("password", password),
+                  sharedPreferences.setString("server", server),
                   Navigator.pop(context),
                 },
                 child: Text(local!.loginLogin),
