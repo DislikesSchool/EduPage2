9b298bef9d0df894f1b9570d1c9b12949eec7f66
vyPal <kubik.palacky@gmail.com>
2023-09-14 18:26:08 +0200

    Message loading fix

--- lib/messages.dart
+++ lib/messages.dart
@@ -106,7 +106,8 @@ class TimeTablePageState extends State<MessagesPage> {
       loading = true; //make loading true to show progressindicator
     });
 
-    apidataMsg = await widget.sessionManager.get('messages');
+    Map<String, dynamic> msgs = await widget.sessionManager.get('messages');
+    apidataMsg = msgs.values.toList();
     messages = getMessages(apidataMsg);
 
     loading = false;
