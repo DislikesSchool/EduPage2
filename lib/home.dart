d88d0f70ca8d70506e98c6062d77094cd75086ef
vyPal <kubik.palacky@gmail.com>
2023-09-23 12:14:44 +0200

    Remove unused import

--- lib/home.dart
+++ lib/home.dart
@@ -13,7 +13,6 @@ import 'package:flutter_session_manager/flutter_session_manager.dart';
 import 'package:package_info/package_info.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:shorebird_code_push/shorebird_code_push.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
