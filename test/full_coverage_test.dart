b4b8c2d682672e097618959b3d87f63e7a7c4ef7
vyPal <kubik.palacky@gmail.com>
2025-04-12 12:06:44 +0200

    Fix testing and remove unused page

--- test/full_coverage_test.dart
+++ test/full_coverage_test.dart
@@ -6,10 +6,14 @@ import 'package:eduapge2/homework.dart';
 import 'package:eduapge2/icanteen.dart';
 import 'package:eduapge2/icanteen_setup.dart';
 import 'package:eduapge2/load.dart';
-import 'package:eduapge2/login.dart';
 import 'package:eduapge2/main.dart';
 import 'package:eduapge2/message.dart';
 import 'package:eduapge2/messages.dart';
 import 'package:eduapge2/timetable.dart';
+import 'package:eduapge2/api.dart';
+import 'package:eduapge2/qrlogin.dart';
+import 'package:eduapge2/search_messages.dart';
+import 'package:eduapge2/screens/onboarding.dart';
+import 'package:eduapge2/components/timer_display.dart';
 
 void main() {}
